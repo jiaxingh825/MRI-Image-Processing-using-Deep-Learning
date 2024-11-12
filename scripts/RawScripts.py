167563dfcc8464dca50b5cc36e1550f23d501bb5
jiaxingh825 <1161602175@qq.com>
2024-11-12 08:37:31 -0500

    1

--- scripts/RawScripts.py
+++ scripts/RawScripts.py
@@ -5,14 +5,6 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
-sys.path.append("C:/code/mri/commonLib")
 import common
 import tensorflow as tf
 import tensorflow.keras.layers as tfl
-
-
-baseline,b = common.readAllAcqs('C:/Active_Noise_Sensing/EMINoise/0827/NoisyImg.h5')
-baseline1,b = common.readAllAcqs('C:/Active_Noise_Sensing/EMINoise/0827/NoisyImgNew.h5')
-#baseline = common.toImg(common.toKSpace(baseline,'E:/JiaxingData/EMINoise/0816/NoiseSignal.h5'))
-print(baseline1[0,0,0:5])
-print(baseline[0,0,0:5])
