b4775e190ebb7bd55afcd34d1c823b0a73a4acfa
jiaxingh825 <Ian.Connell@uhn.ca>
2024-12-04 13:14:53 -0500

    spectrum analysis updates

--- ANS_Scripts/RawScripts.py
+++ ANS_Scripts/RawScripts.py
@@ -9,26 +9,8 @@ import DataProcessing as dp
 import common
 import TrainAndTest as tt
 
-noise = 'E:/JiaxingData/EMINoise/0827/NoiseSignal_32_256.h5'
-img = 'E:/JiaxingData/EMINoise/0827/NoisyImg.h5'
-baseline = 'E:/JiaxingData/EMINoise/0827/BaselineImg.h5'
-
-# Noise channel from  Noisy MRI scans
-noisyImg,b = common.readAllAcqs(img)
-print(noisyImg.shape)
-imageNoise = dp.ConvergeComplexR(dp.SplitComplexR(noisyImg[:,16:18,:]))
-# Noisy Image, image channel from MRI scans
-image = dp.ConvergeComplexR(dp.SplitComplexR(noisyImg[:,0:16,:]))
-# baseline Image
-baseline,b = common.readAllAcqs(baseline)
-baseline = baseline[:,0:16,:]
-baseline = dp.ConvergeComplexR(dp.SplitComplexR(baseline))
-#(16,2)*(2,512) = (16,512)
-# (16,512)*(512,2) = (16,2)
-
-noiseSignal1 = imageNoise
-MRISignal1 = image-baseline
-noisePreScan,b = common.readAllAcqs(img,table_name='noise')
-print(noiseSignal1.shape)
-print(dp.ConvergeComplexR(dp.SplitComplexR(noisePreScan)).shape)
+noisePre,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0827/NoisyImg.h5',table_name="noise")
+print(noisePre.shape)
+noisePre = dp.ConvergeComplexR(dp.SplitComplexR(noisePre))
+print(noisePre.shape)
 
--- ANS_Scripts/spectrumAnalysis.py
+++ ANS_Scripts/spectrumAnalysis.py
@@ -5,36 +5,57 @@ import common
 import DataProcessing as dp
 
 date = '0816'
-noiseOnly,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0816/NoiseSignal.h5')
+noiseOnly,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0827/NoiseSignal_32_256.h5')
 noiseOnly = dp.ConvergeComplexR(dp.SplitComplexR(noiseOnly))
-noiseImage,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0816/NoiseImage.h5')
+noiseImage,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0827/NoisyImg.h5')
 noiseImage = dp.ConvergeComplexR(dp.SplitComplexR(noiseImage))
-baseline,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0816/BaselineImage.h5')
+baseline,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0827/BaselineImg.h5')
 baseline = dp.ConvergeComplexR(dp.SplitComplexR(baseline))
-
-i1 = 0
+flip0,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0827/0Flip.h5')
+flip0 = dp.ConvergeComplexR(dp.SplitComplexR(flip0))
+noisePre,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0827/NoisyImg.h5',table_name="noise")
+noisePre = dp.ConvergeComplexR(dp.SplitComplexR(noisePre))
+noisePre0,b = common.readAllAcqs('E:/JiaxingData/EMINoise/0827/0Flip.h5',table_name="noise")
+noisePre0 = dp.ConvergeComplexR(dp.SplitComplexR(noisePre0))
+i1 = 250
 i2 = 17
 
 noiseOnly = noiseOnly[i1,i2]
 noise = noiseImage[i1,i2]
 noiseImage = noiseImage[i1,i2] - baseline[i1,i2]
-print(noiseOnly.shape)
-print(noiseImage.shape)
-
+flip0 = flip0[i1,i2]
+noisePre = noisePre[0,i2,0:512]
+noisePre0 = noisePre0[0,i2,0:512]
 
 fft1 = np.fft.fft(noiseOnly)
 fft2 = np.fft.fft(noise)
+fft3 = np.fft.fft(flip0)
+fft4 = np.fft.fft(noisePre)
+fft5 = np.fft.fft(noisePre0)
+phase1 = np.angle(fft1)
+phase2 = np.angle(fft2)
+phase3 = np.angle(fft3)
+phase4 = np.angle(fft4)
 ps1 = 20*np.log10(np.abs(fft1)**2)
 ps2 = 20*np.log10(np.abs(fft2)**2)
 
-sampling_frequency1 = common.getRate('E:/JiaxingData/EMINoise/0816/NoiseSignal.h5')
-sampling_frequency2 = common.getRate('E:/JiaxingData/EMINoise/0816/NoiseImage.h5')
+sampling_frequency1 = common.getRate('E:/JiaxingData/EMINoise/0827/NoiseSignal_32_256.h5')
+sampling_frequency2 = common.getRate('E:/JiaxingData/EMINoise/0827/NoisyImg.h5')
+sampling_frequency3 = common.getRate('E:/JiaxingData/EMINoise/0827/0Flip.h5')
 freqs1 = np.fft.fftfreq(len(noiseOnly),1/sampling_frequency1)
-freqs2 = np.fft.fftfreq(len(noiseOnly),1/sampling_frequency2)
+freqs2 = np.fft.fftfreq(len(noise),1/sampling_frequency2)
+freqs3 = np.fft.fftfreq(len(flip0),1/sampling_frequency3)
+freqs4 = np.fft.fftfreq(len(noisePre),1/sampling_frequency2)
+
+
 
 plt.plot(freqs1[:len(freqs1)//2],np.abs(fft1)[:len(freqs1)//2])
 plt.plot(freqs2[:len(freqs2)//2],np.abs(fft2)[:len(freqs2)//2])
-plt.legend(['noise only','noise map'])
-plt.savefig('C:/Users/Admin/Desktop/signalExamination/'+date+'/abs(fft)_'+str(i1)+'_'+str(i2)+'.png')
+plt.plot(freqs3[:len(freqs3)//2],np.abs(fft3)[:len(freqs3)//2])
+plt.plot(freqs2[:len(freqs4)//2],np.abs(fft4)[:len(freqs2)//2])
+plt.plot(freqs3[:len(freqs3)//2],np.abs(fft5)[:len(freqs3)//2])
+plt.grid()
+plt.legend(['noise only','noise map','flip Angle 0','regular noise preScan', '0FlipAngle noise preScan'])
+#plt.savefig('C:/Users/Admin/Desktop/signalExamination/'+date+'/abs(fft)_'+str(i1)+'_'+str(i2)+'.png')
 #plt.plot(freqs[:len(freqs)//2],np.abs(fft)[:len(freqs)//2])
 plt.show()
